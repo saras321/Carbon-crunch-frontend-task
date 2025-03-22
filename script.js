a768410bf1ef879f08091dd3c4b4742c344c730e
saras321 <sarasdevi2005@gmail.com>
2025-03-22 15:53:33 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+document.querySelector(".login-btn").addEventListener("click", function () {
+  alert("Login feature coming soon!");
+});
+document.querySelector(".demo-btn").addEventListener("click", function () {
+  alert("Booking a demo is not available right now. Stay tuned!");
+});
+document.querySelector(".calc-btn").addEventListener("click", function () {
+  alert("Free Carbon Calculator is under development.");
+});
+document.querySelector(".second-demo").addEventListener("click", function () {
+  alert("Free Carbon Calculator is under development.");
+});
